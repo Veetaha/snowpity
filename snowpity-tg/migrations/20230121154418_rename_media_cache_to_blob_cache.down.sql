007751c3a7394354a8954944cadace15571dcad4
Veetaha <gersoh3@gmail.com>
2023-01-28 01:26:20 +0100

    Add reversible migration

---
+++ snowpity-tg/migrations/20230121154418_rename_media_cache_to_blob_cache.down.sql
@@ -0,0 +1,9 @@
+alter table tg_twitter_blob_cache
+rename constraint tg_twitter_blob_cache_pk to tg_twitter_media_cache_pk;
+
+alter table tg_twitter_blob_cache rename to tg_twitter_media_cache;
+
+alter table tg_derpibooru_blob_cache
+rename constraint tg_derpibooru_blob_cache_pk to tg_derpi_media_cache_pk;
+
+alter table tg_derpibooru_blob_cache rename to tg_derpi_media_cache;
